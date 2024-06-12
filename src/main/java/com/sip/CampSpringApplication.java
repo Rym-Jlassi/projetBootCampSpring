002542c1a13fdf8eb1c547afcf067ea29cf16589
Rym Jlassi <jlassi.rim1121@gmail.com>
2024-06-12 09:13:52 +0100

    Mise a jour dans le fichier main

--- src/main/java/com/sip/CampSpringApplication.java
+++ src/main/java/com/sip/CampSpringApplication.java
@@ -1,18 +1,14 @@
 package com.sip;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.sip.entities.Provider;
-import com.sip.repositories.ProviderRepository;
 
 @SpringBootApplication
 public class CampSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CampSpringApplication.class, args);
-		
+		System.out.println("Camp devops Started");
 		
 	}
 
